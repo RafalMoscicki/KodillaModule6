b53336689eb8b4efcdfd8b09f5a533dce29563ae
RafalMoscicki <60443383+RafalMoscicki@users.noreply.github.com>
2020-02-19 22:20:02 +0100

    kod.7.3.zadanie (#8)
    kod.7.3: funkcyjny spacer po liście użytkowników forum

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -0,0 +1,21 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forum {
+    private final List<ForumUser> userList;
+
+    public Forum() {
+        userList = new ArrayList<>();
+        userList.add(new ForumUser(001, "Adam", 'M', LocalDate.of(1990, 12, 31), 10));
+        userList.add(new ForumUser(001, "Liz", 'F', LocalDate.of(1991, 10, 12), 32));
+        userList.add(new ForumUser(001, "Pixel", 'M', LocalDate.of(2018, 11, 11), 100));
+        userList.add(new ForumUser(001, "Kim", 'F', LocalDate.of(1999, 1, 5), 0));
+    }
+
+    public List<ForumUser> getUserList() {
+        return new ArrayList<>(userList);
+    }
+}
