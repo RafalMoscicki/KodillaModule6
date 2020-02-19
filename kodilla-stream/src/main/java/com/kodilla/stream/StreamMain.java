3dd7382b8aff23ffecb5485aaee269e8443aa6f2
RafalMoscicki <60443383+RafalMoscicki@users.noreply.github.com>
2020-02-19 22:18:07 +0100

    kod.7.3: poprawki po code review

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -11,13 +11,13 @@ import java.util.stream.Collectors;
 public class StreamMain {
     public static void main(String[] args) {
         Forum forum = new Forum();
-        Map<Integer, ForumUser> collect = forum.getUserList().stream()
+        Map<Integer, ForumUser> filteredUsers = forum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
                 .filter(forumUser -> ChronoUnit.YEARS.between(forumUser.getDateOfBirth(), LocalDate.now()) >= 20)
                 .filter(forumUser -> forumUser.getPostNumber() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
 
-        collect.entrySet().stream()
+        filteredUsers.entrySet().stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
     }
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Forum {
-    private final List<ForumUser> userList = new ArrayList<>();
+    private final List<ForumUser> userList;
 
     public Forum() {
+        userList = new ArrayList<>();
         userList.add(new ForumUser(001, "Adam", 'M', LocalDate.of(1990, 12, 31), 10));
         userList.add(new ForumUser(001, "Liz", 'F', LocalDate.of(1991, 10, 12), 32));
         userList.add(new ForumUser(001, "Pixel", 'M', LocalDate.of(2018, 11, 11), 100));
